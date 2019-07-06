c83d3f42f7c520aad8da1ed034077385680c1146
koa2019 <no-reply>
2019-07-06 12:09:34 -0700

    fixed getWins & getLoss by changing it to .text() instead of append(). Bug: gems not randomizing after win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
-//Declare & initialize variables
-const randNumMin = 9;
-const randNumMax = 120;
-const gemMin = 1;
-const gemMax = 12;
-
-// var win = 0;
-// var loss = 0;
-var numWins = 0;
-var numLoss = 0;
-var total = 0;
-// var totalScore = 0;
-// var num1 = 0;
-// var num2 = 0;
-var randGemVal = 0;
-var gemVal = 0;
-// var gemArr = [];
-var gemImage;
-var randNum = 0;
-
-
-
 $(document).ready(function () {
+
+    //Declare & initialize variables
+    const randNumMin = 9;
+    const randNumMax = 120;
+    const gemMin = 1;
+    const gemMax = 12;
+
+    // var win = 0;
+    // var loss = 0;
+    var numWins = 0;
+    var numLoss = 0;
+    var total = 0;
+    // var totalScore = 0;
+    // var num1 = 0;
+    // var num2 = 0;
+    var randGemVal = 0;
+    var gemVal = 0;
+    // var gemArr = [];
+    var gemImage;
+    var randNum = 0;
+
+
+
 
     displayGame();
     if (total === randNum) {
@@ -29,15 +30,15 @@ $(document).ready(function () {
     else {
         startGame();
     }
-    
-    
+
+
     function displayGame() {
 
         getRandNum(randNumMin, randNumMax);
         $("#win").append(numWins);
         $("#loss").append(numLoss);
         $("#totalScore").append(total);
-             
+
     }
 
     function startGame() {
@@ -48,37 +49,38 @@ $(document).ready(function () {
             gemImage.attr('data-gem-value', randGemVal);  //create gemImage attr: datat-gem-value & assign randGemVal
             gemImage.attr('src', "./assets/images/gem_green-200x200.jpg")
             $("#gems").append(gemImage);
-        
+
             gemImage.on('click', function () {
                 gemVal = $(this).attr('data-gem-value');
                 console.log("You clicked a gem " + gemVal);
-    
-                console.log("total before getTotal(): " + total);
-                 
+
+                // console.log("total before getTotal(): " + total);
+                total = parseInt(total);
+                gemVal = parseInt(gemVal);
+
                 total = getTotal(total, gemVal);
                 console.log("total after getTotal(): " + total);
-                $("#totalScore").append(total);
+                $("#totalScore").text(total);
 
-                if (total === randNum){
+                if (total === randNum) {
                     getWins();
                     getRandNum(randNumMin, randNumMax);
-                    alert("Winner! Click gem to play again.")
-                    total = 0;
+                    console.log("Winner! Click gem to play again.");
 
                 }
                 else if (total > randNum) {
                     getLoss();
+                    console.log("randMin " + randNumMin + " randMax " + randNumMax);
                     getRandNum(randNumMin, randNumMax);
-                    alert("Loser! Click gem to play again.")
-                    total = 0;
+                    resetGame();
 
-                }              
+                }
             })
         }
     }
-            
-        
-    
+
+
+
 
 
     function getRandGemVal(gemMin, gemMax) {
@@ -87,27 +89,34 @@ $(document).ready(function () {
 
     function getRandNum(randNumMin, RandNumMax) {
         randNum = Math.floor(Math.random() * (RandNumMax - randNumMin) + 1) + randNumMin;
-        $("#randomNumber").append(randNum);
+        $("#randomNumber").text(randNum);
+        console.log("randNum in getRandNum(): " + randNum);
     }
 
-    function getTotal(total, gemVal){
+    function getTotal(total, gemVal) {
         total += gemVal;
-        console.log("gemVal inside getTotal() " + gemVal);
-        console.log("total inside getTotal() " + total);
+        // console.log("gemVal inside getTotal() " + gemVal);
+        // console.log("total inside getTotal() " + total);
         return total;
 
     }
     function getWins() {
         numWins++;
         $("#win").append(numWins);
-        
+
     }
 
     function getLoss() {
         numLoss++;
-        $("#loss").append(numLoss);
+        $("#loss").text(numLoss);
+        console.log("BUST! Click any gem to play again.");
+        
     }
-
+    function resetGame() {
+        total = 0 ;
+        $("#totalScore").text(total);
+        console.log("total inside getLoss(): " + total);
+    }
 
 
 });  //closes $(document).ready
